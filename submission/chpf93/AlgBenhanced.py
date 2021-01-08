5385efc728e209f317aab2b09b29594f199b3848
fboyle2001 <finlayboyle2001@gmail.com>
2021-01-08 20:05:42 +0000

    First submission draft ready

--- submission/chpf93/AlgBenhanced.py
+++ submission/chpf93/AlgBenhanced.py
@@ -146,7 +146,7 @@ def read_in_algorithm_codes_and_tariffs(alg_codes_file):
 ############ THE CITY FILE IS IN THE FOLDER 'city-files'.
 ############
 
-input_file = "AISearchfile058.txt"
+input_file = "AISearchfile012.txt"
 
 ############
 ############ PLEASE SCROLL DOWN UNTIL THE NEXT BLOCK OF CAPITALIZED COMMENTS.
@@ -425,91 +425,86 @@ def ant_colony_optimise(tau_nought, initial_tour, ants, max_it, alpha, beta, rho
 
     return best_tour, best_tour_length
 
-if __name__ == "__main__":
-    nn_tour = nearest_neighbour(0)
-    nn_tour_length = tour_length_calc(nn_tour)
-    ants = num_cities
-    max_it = 50000 // ants
-    w = 20
-    rho = 0.4
-    tau_nought = (0.5 * w * (w - 1)) / (rho * nn_tour_length)
-    alpha = 2.2
-    beta = 13
+nn_tour = nearest_neighbour(0)
+nn_tour_length = tour_length_calc(nn_tour)
+ants = num_cities
+max_it = 20000 // ants
+w = 6
+rho = 0.1
+tau_nought = (0.5 * w * (w - 1)) / (rho * nn_tour_length)
+alpha = 1
+beta = 3.5
 
-    added_note = f"max_it = {max_it}, ants = {num_cities}, alpha = {alpha}, beta = {beta}, rho = {rho}, tau_nought = {tau_nought}, w = {w}"
+added_note = f"max_it = {max_it}, ants = {num_cities}, alpha = {alpha}, beta = {beta}, rho = {rho}, tau_nought = {tau_nought}, w = {w}"
 
-    time_start = time.time()
-    tour, tour_length = ant_colony_optimise(tau_nought, nn_tour, ants, max_it, alpha, beta, rho, w)
-    time_end = time.time()
+tour, tour_length = ant_colony_optimise(tau_nought, nn_tour, ants, max_it, alpha, beta, rho, w)
 
-    print(time_end - time_start)
+############
+############ YOUR CODE SHOULD NOW BE COMPLETE AND WHEN EXECUTION OF THIS PROGRAM 'skeleton.py'
+############ REACHES THIS POINT, YOU SHOULD HAVE COMPUTED A TOUR IN THE RESERVED LIST VARIABLE 'tour',
+############ WHICH HOLDS A LIST OF THE INTEGERS FROM {0, 1, ..., 'num_cities' - 1}, AND YOU SHOULD ALSO
+############ HOLD THE LENGTH OF THIS TOUR IN THE RESERVED INTEGER VARIABLE 'tour_length'.
+############
 
-    ############
-    ############ YOUR CODE SHOULD NOW BE COMPLETE AND WHEN EXECUTION OF THIS PROGRAM 'skeleton.py'
-    ############ REACHES THIS POINT, YOU SHOULD HAVE COMPUTED A TOUR IN THE RESERVED LIST VARIABLE 'tour',
-    ############ WHICH HOLDS A LIST OF THE INTEGERS FROM {0, 1, ..., 'num_cities' - 1}, AND YOU SHOULD ALSO
-    ############ HOLD THE LENGTH OF THIS TOUR IN THE RESERVED INTEGER VARIABLE 'tour_length'.
-    ############
+############
+############ YOUR TOUR WILL BE PACKAGED IN A TOUR FILE OF THE APPROPRIATE FORMAT AND THIS TOUR FILE,
+############ WHOSE NAME WILL BE A MIX OF THE NAME OF THE CITY FILE, THE NAME OF THIS PROGRAM AND THE
+############ CURRENT DATA AND TIME. SO, EVERY SUCCESSFUL EXECUTION GIVES A TOUR FILE WITH A UNIQUE
+############ NAME AND YOU CAN RENAME THE ONES YOU WANT TO KEEP LATER.
+############
 
-    ############
-    ############ YOUR TOUR WILL BE PACKAGED IN A TOUR FILE OF THE APPROPRIATE FORMAT AND THIS TOUR FILE,
-    ############ WHOSE NAME WILL BE A MIX OF THE NAME OF THE CITY FILE, THE NAME OF THIS PROGRAM AND THE
-    ############ CURRENT DATA AND TIME. SO, EVERY SUCCESSFUL EXECUTION GIVES A TOUR FILE WITH A UNIQUE
-    ############ NAME AND YOU CAN RENAME THE ONES YOU WANT TO KEEP LATER.
-    ############
+############
+############ DO NOT TOUCH OR ALTER THE CODE BELOW THIS POINT! YOU HAVE BEEN WARNED!
+############
 
-    ############
-    ############ DO NOT TOUCH OR ALTER THE CODE BELOW THIS POINT! YOU HAVE BEEN WARNED!
-    ############
+flag = "good"
+length = len(tour)
+for i in range(0, length):
+    if isinstance(tour[i], int) == False:
+        flag = "bad"
+    else:
+        tour[i] = int(tour[i])
+if flag == "bad":
+    print("*** error: Your tour contains non-integer values.")
+    sys.exit()
+if isinstance(tour_length, int) == False:
+    print("*** error: The tour-length is a non-integer value.")
+    sys.exit()
+tour_length = int(tour_length)
+if len(tour) != num_cities:
+    print("*** error: The tour does not consist of " + str(num_cities) + " cities as there are, in fact, " + str(len(tour)) + ".")
+    sys.exit()
+flag = "good"
+for i in range(0, num_cities):
+    if not i in tour:
+        flag = "bad"
+if flag == "bad":
+    print("*** error: Your tour has illegal or repeated city names.")
+    sys.exit()
+check_tour_length = 0
+for i in range(0, num_cities - 1):
+    check_tour_length = check_tour_length + dist_matrix[tour[i]][tour[i + 1]]
+check_tour_length = check_tour_length + dist_matrix[tour[num_cities - 1]][tour[0]]
+if tour_length != check_tour_length:
+    flag = print("*** error: The length of your tour is not " + str(tour_length) + "; it is actually " + str(check_tour_length) + ".")
+    sys.exit()
+print("You, user " + my_user_name + ", have successfully built a tour of length " + str(tour_length) + "!")
 
-    flag = "good"
-    length = len(tour)
-    for i in range(0, length):
-        if isinstance(tour[i], int) == False:
-            flag = "bad"
-        else:
-            tour[i] = int(tour[i])
-    if flag == "bad":
-        print("*** error: Your tour contains non-integer values.")
-        sys.exit()
-    if isinstance(tour_length, int) == False:
-        print("*** error: The tour-length is a non-integer value.")
-        sys.exit()
-    tour_length = int(tour_length)
-    if len(tour) != num_cities:
-        print("*** error: The tour does not consist of " + str(num_cities) + " cities as there are, in fact, " + str(len(tour)) + ".")
-        sys.exit()
-    flag = "good"
-    for i in range(0, num_cities):
-        if not i in tour:
-            flag = "bad"
-    if flag == "bad":
-        print("*** error: Your tour has illegal or repeated city names.")
-        sys.exit()
-    check_tour_length = 0
-    for i in range(0, num_cities - 1):
-        check_tour_length = check_tour_length + dist_matrix[tour[i]][tour[i + 1]]
-    check_tour_length = check_tour_length + dist_matrix[tour[num_cities - 1]][tour[0]]
-    if tour_length != check_tour_length:
-        flag = print("*** error: The length of your tour is not " + str(tour_length) + "; it is actually " + str(check_tour_length) + ".")
-        sys.exit()
-    print("You, user " + my_user_name + ", have successfully built a tour of length " + str(tour_length) + "!")
+local_time = time.asctime(time.localtime(time.time()))
+output_file_time = local_time[4:7] + local_time[8:10] + local_time[11:13] + local_time[14:16] + local_time[17:19]
+output_file_time = output_file_time.replace(" ", "0")
+script_name = os.path.basename(sys.argv[0])
+if len(sys.argv) > 2:
+    output_file_time = sys.argv[2]
+output_file_name = script_name[0:len(script_name) - 3] + "_" + input_file[0:len(input_file) - 4] + "_" + output_file_time + ".txt"
 
-    local_time = time.asctime(time.localtime(time.time()))
-    output_file_time = local_time[4:7] + local_time[8:10] + local_time[11:13] + local_time[14:16] + local_time[17:19]
-    output_file_time = output_file_time.replace(" ", "0")
-    script_name = os.path.basename(sys.argv[0])
-    if len(sys.argv) > 2:
-        output_file_time = sys.argv[2]
-    output_file_name = script_name[0:len(script_name) - 3] + "_" + input_file[0:len(input_file) - 4] + "_" + output_file_time + ".txt"
-
-    f = open(output_file_name,'w')
-    f.write("USER = " + my_user_name + " (" + my_first_name + " " + my_last_name + "),\n")
-    f.write("ALGORITHM CODE = " + algorithm_code + ", NAME OF CITY-FILE = " + input_file + ",\n")
-    f.write("SIZE = " + str(num_cities) + ", TOUR LENGTH = " + str(tour_length) + ",\n")
-    f.write(str(tour[0]))
-    for i in range(1,num_cities):
-        f.write("," + str(tour[i]))
-    f.write(",\nNOTE = " + added_note)
-    f.close()
-    print("I have successfully written your tour to the tour file:\n   " + output_file_name + ".")
+f = open(output_file_name,'w')
+f.write("USER = " + my_user_name + " (" + my_first_name + " " + my_last_name + "),\n")
+f.write("ALGORITHM CODE = " + algorithm_code + ", NAME OF CITY-FILE = " + input_file + ",\n")
+f.write("SIZE = " + str(num_cities) + ", TOUR LENGTH = " + str(tour_length) + ",\n")
+f.write(str(tour[0]))
+for i in range(1,num_cities):
+    f.write("," + str(tour[i]))
+f.write(",\nNOTE = " + added_note)
+f.close()
+print("I have successfully written your tour to the tour file:\n   " + output_file_name + ".")
